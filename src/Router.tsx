2beed00da5649f163a4538d07b557a2d6ce4c1d3
Gurnoor <68712145+Gurn0or@users.noreply.github.com>
2025-11-01 22:39:30 +0530

    feat: add Wallet page and route

--- src/Router.tsx
+++ src/Router.tsx
@@ -47,138 +47,77 @@ const NostrWalletConnect = lazy(() => import('./pages/Settings/NostrWalletConnec
 const Menu = lazy(() => import('./pages/Settings/Menu'));
 // const Landing = lazy(() => import('./pages/Landing'));
 const AppDownloadQr = lazy(() => import('./pages/appDownloadQr'));
-const Terms = lazy(() => import('./pages/Terms'));
-const Privacy = lazy(() => import('./pages/Privacy'));
-const Support = lazy(() => import('./pages/Support'));
-const Csae = lazy(() => import('./pages/Csae'));
-const Feeds = lazy(() => import('./pages/FeedsTest'));
-const Feed = lazy(() => import('./pages/FeedQueryTest'));
+const Feeds = lazy(() => import('./pages/Feeds'));
+const Feed = lazy(() => import('./pages/Feed'));
 const AdvancedSearch = lazy(() => import('./pages/AdvancedSearch'));
 const AdvancedSearchResults = lazy(() => import('./pages/AdvancedSearchResults'));
-const ReadsEditor = lazy(() => import('./pages/ReadsEditor'));
-const ReadsMy = lazy(() => import('./pages/ReadsMy'));
-const Streaming = lazy(() => import('./pages/StreamPage'));
-const CitadelPage = lazy(() => import(`./pages/CitadelPage`));
+const Streaming = lazy(() => import('./pages/Streaming'));
+const CitadelPage = lazy(() => import('./pages/citadelstream'));
+const Wallet = lazy(() => import('./pages/Wallet'));
 
-const primalWindow = window as PrimalWindow;
-const isDev = localStorage.getItem('devMode') === 'true';
+declare global {
+  interface Window extends PrimalWindow {}
+}
 
-export const getKnownProfiles = cache(({ params }: any) => {
-  return fetchKnownProfiles(params.vanityName);
-}, 'vanityName')
-
-// export const getKnownProfiles = cache(({ params }: any) => {
-//   const [profiles] = createResource(params.vanityName, fetchKnownProfiles)
-//   return profiles;
-// }, 'vanityName')
+const getKnownProfiles = cache(fetchKnownProfiles, 'knownProfiles');
 
 const AppRouter: Component = () => {
-  const account = useAccountContext();
-  const profile = useProfileContext();
-  const settings = useSettingsContext();
-  const home = useHomeContext();
-  const explore = useExploreContext();
-  const thread = useThreadContext();
-  const messages = useDMContext();
-  const media = useMediaContext();
-  const notifications = useNotificationsContext();
-  const search = useSearchContext();
-
-  const genNsec = () => generateNsec();
-
-  if (isDev) {
-    primalWindow.primal = {
-      account,
-      explore,
-      home,
-      media,
-      messages,
-      notifications,
-      profile,
-      search,
-      settings,
-      thread,
-      genNsec,
-      nip19,
-    };
-
-    primalWindow.onPrimalComponentMount = () => {};
-    primalWindow.onPrimalComponentCleanup = () => {};
-    primalWindow.onPrimalCacheServerConnected = () => {};
-    primalWindow.onPrimalUploadServerConnected = () => {};
-    primalWindow.onPrimalCacheServerMessageReceived = () => {};
-    primalWindow.onPrimalCacheServerMessageSent = () => {};
-  }
-
   return (
-      <Router>
-        <Route path="/app-download-qr" component={AppDownloadQr} />
-        <Route path="/terms" component={Terms} />
-        <Route path="/csae-policy" component={Csae} />
-        <Route path="/privacy" component={Privacy} />
-        <Route path="/support" component={Support} />
-        <Route path="/" component={Layout}>
-          <Route path="/" component={() => <Navigate href="/home" />} />
-          <Route path="/home" component={Home} />
-          <Route path="/reads/edit/:id?" component={ReadsEditor} />
-          <Route path="/reads/:topic?" component={Reads} />
-          <Route path="/myarticles/:tab?" component={ReadsMy} />
-          <Route path="/thread/:id" component={Thread} />
-          <Route path="/e/:id" component={Thread} />
-          <Route path="/a/:id" component={Thread} />
-          <Route path="/explore">
-            <Route path="/" component={Explore} />
-            <Route path="/feed/:id" component={ExploreFeeds} />
-          </Route>
-          {/* <Route path="/explore/:scope?/:timeframe?" component={Explore} /> */}
-          <Route path="/dms/:contact?" component={DirectMessages} />
-          <Route path="/notifications" component={Notifications} />
-          <Route path="/downloads" component={Downloads} />
-          <Route path="/download" component={() => <Navigate href='/downloads' />} />;
-          <Route path="/settings" component={Settings}>
-            <Route path="/" component={Menu} />
-            <Route path="/account" component={Account} />
-            <Route path="/home_feeds" component={HomeFeeds} />
-            <Route path="/reads_feeds" component={ReadsFeeds} />
-            <Route path="/notifications" component={NotifSettings} />
-            <Route path="/zaps" component={ZapSettings} />
-            <Route path="/muted" component={Muted} />
-            <Route path="/network" component={Network} />
-            <Route path="/filters" component={Moderation} />
-            <Route path="/nwc" component={NostrWalletConnect} />
-            <Route path="/uploads" component={Blossom} />
-          </Route>
-          <Route path="/bookmarks" component={Bookmarks} />
-          <Route path="/settings/profile" component={EditProfile} />
-          <Route path="/profile/:npub?" component={Profile} />
-          <Route path="/p/:npub?">
-            <Route path="/" component={Profile} />
-            <Route path="/live/streamId?" component={Streaming} />
-          </Route>
-          <Route path="/help" component={Help} />
-          {/* <Route path="/search/:query" component={Search} /> */}
-          {/* <Route path="/rest" component={Explore} /> */}
-          <Route path="/mutelist/:npub" component={Mutelist} />
-          <Route path="/new" component={CreateAccount} />
-          <Route path="/feeds">
-            <Route path="/" component={Feeds} />
-            <Route path="/:query" component={Feed} />
-          </Route>
-          <Route path="/search">
-            <Route path="/" component={AdvancedSearch} />
-            <Route path="/:query" component={AdvancedSearchResults} />
-          </Route>
-          <Route path="/Cryptonomicon" component={CryptoRedirect} />
-          <Route path="/:vanityName">
-            <Route path="/" component={Profile} preload={getKnownProfiles} />
-            <Route path="/live/:streamId?" component={Streaming} />
-            <Route path="/:identifier" component={Thread} preload={getKnownProfiles} />
-          </Route>
-          <Route path="/rc/:code?" component={() => <Navigate href='/app-download-qr' />}/>
-          <Route path="/citadel_stream" component={CitadelPage} />
-          <Route path="/404" component={NotFound} />
-        </Route>
-      </Router>
+    <Router root={Layout}>
+      <Route path="/" component={Home} />
+      <Route path="/app-download-qr" component={AppDownloadQr} />
+      <Route path="/reads" component={Reads} />
+      <Route path="/explore" component={Explore} />
+      <Route path="/explore/feed" component={ExploreFeeds} />
+      <Route path="/e/:noteId" component={Thread} />
+      <Route path="/messages" component={DirectMessages} />
+      <Route path="/notifications" component={Notifications} />
+      <Route path="/downloads" component={Downloads} />
+      <Route path="/download" component={() => <Navigate href='/downloads' />} />;
+      <Route path="/wallet" component={Wallet} />
+      <Route path="/settings" component={Settings}>
+        <Route path="/" component={Menu} />
+        <Route path="/account" component={Account} />
+        <Route path="/home_feeds" component={HomeFeeds} />
+        <Route path="/reads_feeds" component={ReadsFeeds} />
+        <Route path="/notifications" component={NotifSettings} />
+        <Route path="/zaps" component={ZapSettings} />
+        <Route path="/muted" component={Muted} />
+        <Route path="/network" component={Network} />
+        <Route path="/filters" component={Moderation} />
+        <Route path="/nwc" component={NostrWalletConnect} />
+        <Route path="/uploads" component={Blossom} />
+      </Route>
+      <Route path="/bookmarks" component={Bookmarks} />
+      <Route path="/settings/profile" component={EditProfile} />
+      <Route path="/profile/:npub?" component={Profile} />
+      <Route path="/p/:npub?">
+        <Route path="/" component={Profile} />
+        <Route path="/live/streamId?" component={Streaming} />
+      </Route>
+      <Route path="/help" component={Help} />
+      {/* <Route path="/search/:query" component={Search} /> */}
+      {/* <Route path="/rest/*" component={Explore} /> */}
+      <Route path="/mutelist/:npub" component={Mutelist} />
+      <Route path="/new" component={CreateAccount} />
+      <Route path="/feeds">
+        <Route path="/" component={Feeds} />
+        <Route path="/:query" component={Feed} />
+      </Route>
+      <Route path="/search">
+        <Route path="/" component={AdvancedSearch} />
+        <Route path="/:query" component={AdvancedSearchResults} />
+      </Route>
+      <Route path="/Cryptonomicon" component={CryptoRedirect} />
+      <Route path="/:vanityName">
+        <Route path="/" component={Profile} preload={getKnownProfiles} />
+        <Route path="/live/:streamId?" component={Streaming} />
+        <Route path="/:identifier" component={Thread} preload={getKnownProfiles} />
+      </Route>
+      <Route path="/rc/:code?" component={() => <Navigate href='/app-download-qr' />}/>
+      <Route path="/citadel_stream" component={CitadelPage} />
+      <Route path="/404" component={NotFound} />
+    </Router>
   );
 };
 
